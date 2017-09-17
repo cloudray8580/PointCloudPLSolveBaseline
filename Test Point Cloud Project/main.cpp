3a7380e7fa60d5f61748d670f1782b22b1cb9e8e
李喆 <lizhe@lizhedemacbook-pro.local>
2017-09-17 21:40:21 +0800

    Initial Commit

---
+++ Test Point Cloud Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test Point Cloud Project
+//
+//  Created by 李喆 on 2017/9/17.
+//  Copyright © 2017年 李喆. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
